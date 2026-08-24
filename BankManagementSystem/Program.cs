48ab9368894dcb5bf27b4c8fd8bede4ba7eb87a9
aiman almoliki <aimnalmoliki072@gmail.com>
2026-08-24 23:08:34 +0300

    feat: implement client listing and add client flow

--- BankManagementSystem/Program.cs
+++ BankManagementSystem/Program.cs
@@ -1,4 +1,65 @@
-﻿static void GoBackToMainMenue()
+﻿
+
+static void AddNewClient()
+{
+    sClient client = ReadNewClient();
+
+    AddDataLineToFile(
+        ClientsFileName,
+        ConvertRecordToLine(client)
+    );
+}
+
+static void AddNewClients()
+{
+    char AddMore = 'y';
+    do
+    {
+        Conosle.WriteLine("adding New Client :");
+        AddNewClient();
+        Conosle.WriteLine("client added successfully,do you want to add more clients? Y/N? ");
+        AddMore = Console.ReadLine();
+
+
+    }
+}
+
+ShowAddNewClientsScreen(){
+    Conosle.WriteLine("===============================================");
+    Conosle.WriteLine("Add New Clients Screen : ");
+    Conosle.WriteLine("===============================================");
+    AddNewClients();
+
+}
+
+static void ShowAllClientsScreen()
+{
+    List<sClient> clients = LoadClientsDataFromFile(clientsFileName);
+    Conosle.WriteLine("\t\t\t\t\t list clint : "+clients.Count()+" clints");
+    Conosle.WriteLine("===============================================");
+    Console.WriteLine(
+    $"| {"Account Number",-15}" +
+    $"| {"Pin Code",-10}" +
+    $"| {"Client Name",-40}" +
+    $"| {"Phone",-12}" +
+    $"| {"Balance",-12}|"
+);
+    Conosle.WriteLine("===============================================");
+    if (clients.Count() == 0)
+        Conosle.WriteLine("no avilable client in the systme");
+    else
+        foreach (sClient Client in clients)
+        {
+            printclientrecordlient(Client);
+        }
+
+    Conosle.WriteLine("===============================================");
+    Conosle.WriteLine("===============================================");
+
+
+
+}
+static void GoBackToMainMenue()
 {
     Conosle.WriteLine("prese any key to go back to main menue... ");
     System("pause>0");
